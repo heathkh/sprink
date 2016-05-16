e17ad5ea1fdf79df964b3a84a4624acf309c66df
Kyle Heath <kyle@mousera.com>
2016-05-15 18:04:06 -0700

    wip

--- manual.py
+++ manual.py
@@ -5,14 +5,14 @@ import cli
 
 FLAGS = gflags.FLAGS
 gflags.DEFINE_integer('zone', None, 'ID of the zone')
-gflags.DEFINE_boolean('on', True, 'Desired state of the zone')
+gflags.DEFINE_boolean('state', True, 'Desired state of the zone')
 
 gflags.MarkFlagAsRequired('zone')
 
 def main():
     cli.init()
     sprink = driver.SprinklerDriver()
-    sprink.set_zone(FLAGS.zone, FLAGS.on)
+    sprink.set_zone(FLAGS.zone, FLAGS.state)
     print sprink.get_zone_states()
     return
 
